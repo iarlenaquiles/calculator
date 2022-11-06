851e0881f00e35abd3d00afc0dba13ff2fd08379
Iarlen Aquiles <iarlensilva775@gmail.com>
2022-11-06 18:11:48 -0300

    created components

--- App.js
+++ App.js
@@ -1,108 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import Button from './src/components/Button';
+import Display from './src/components/Display';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [displayValue, setDisplayValue] = useState('100');
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleClick = number => {
+    setDisplayValue(number);
   };
 
+  const clearMemory = () => {
+    setDisplayValue('0');
+  };
+
+  const setOperation = operation => {};
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Display value={displayValue} />
+      <View style={styles.buttons}>
+        <Button label="AC" triple onClick={clearMemory} />
+        <Button label="/" operation onClick={setOperation} />
+        <Button label="7" onClick={handleClick} />
+        <Button label="8" onClick={handleClick} />
+        <Button label="9" onClick={handleClick} />
+        <Button label="*" operation onClick={setOperation} />
+        <Button label="4" onClick={handleClick} />
+        <Button label="5" onClick={handleClick} />
+        <Button label="6" onClick={handleClick} />
+        <Button label="-" operation onClick={setOperation} />
+        <Button label="1" onClick={handleClick} />
+        <Button label="2" onClick={handleClick} />
+        <Button label="3" onClick={handleClick} />
+        <Button label="+" operation onClick={setOperation} />
+        <Button label="0" double onClick={handleClick} />
+        <Button label="." onClick={handleClick} />
+        <Button label="=" operation onClick={setOperation} />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
   sectionDescription: {
     marginTop: 8,
